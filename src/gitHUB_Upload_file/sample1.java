10025ed0dd620978ac993eef8c85c09f0d9e38ec
CHICKU <CHICKU@DESKTOP-B49NLJV>
2022-08-28 14:18:24 -0500

    (new program updated)sample1

---
+++ src/gitHUB_Upload_file/sample1.java
@@ -0,0 +1,9 @@
+package gitHUB_Upload_file;
+
+public class sample1 {
+public static void main(String[] args) {
+	System.out.println("akshay");
+	System.out.println("kallu");
+	System.out.println("Public");
+}
+}
