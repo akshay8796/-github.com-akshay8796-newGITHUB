07a57c1462ce804cd098faef0f64880c3a024d41
CHICKU <CHICKU@DESKTOP-B49NLJV>
2022-08-28 14:26:19 -0500

    (new program)sample2

---
+++ src/gitHUB_Upload_file/sample2.java
@@ -0,0 +1,11 @@
+package gitHUB_Upload_file;
+
+public class sample2 
+{
+public static void main(String[] args)
+{
+System.out.println("hardware");
+System.out.println("software");
+System.out.println("compitability");
+}
+}
